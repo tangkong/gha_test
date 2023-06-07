c26effcdd2bc701c322fc648be004eb4deed9044
tangkong <tangkongrobert@gmail.com>
2023-06-07 09:28:56 -0700

    MNT: change script slightly

--- scripts/convert.py
+++ scripts/convert.py
@@ -3,13 +3,17 @@ from pathlib import Path
 
 
 def main(args):
-    dir = Path(args.dir)
+    source_dir = Path(args.csv_dir)
+    xml_dir = Path(args.xml_dir)
 
-    print(list(dir.glob('*')))
+    for item in source_dir.glob('*'):
+        print(item)
 
+    print(xml_dir)
 
 if __name__ == "__main__":
     print('convert script run')
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir')
+    parser.add_argument('csv_dir')
+    parser.add_argument('xml_dir')
     main(parser.parse_args())
